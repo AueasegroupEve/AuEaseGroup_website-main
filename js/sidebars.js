26808f6cb6ef4eb5526a1d07103822043ca586b6
samztz <1196572086@qq.com>
2023-04-18 14:18:52 +1000

    update navigation UI

--- js/sidebars.js
+++ js/sidebars.js
@@ -7,4 +7,15 @@
   })
 })()
 
+let mybuttons = document.getElementsByClassName("blog-pagination");
+const mybutton = mybuttons[0];
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";let
+  }
+}
